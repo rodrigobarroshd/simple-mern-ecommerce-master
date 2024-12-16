4560d0f5d79904ad86d15e512d0a0600690752f7
Rodrigo Barros <brodrigo87@gmail.com>
2024-12-16 18:43:29 -0300

    test

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({ origin: 'https://ecommercesite-3a5b.onrender.com' }))
 app.use(fileUpload({
     useTempFiles: true
 }))
